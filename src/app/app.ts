e575b731505c5f985573f4ec9439d93bdb5986e9
aadhya-arora <aadhya270805@gmail.com>
2025-08-12 18:03:40 +0530

    basic counter to practice angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,28 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('counter');
+  count: number = 0;
+  // handleIncrement() {
+  //   this.count += 1;
+  // }
+  // handleDecrement() {
+  //   this.count -= 1;
+  // }
+  // handleReset() {
+  //   this.count = 0;
+  // }
+
+  handleCounter(val: string) {
+    if (val == 'minus') {
+      this.count -= 1;
+    } else if (val == 'plus') {
+      this.count += 1;
+    } else {
+      this.count = 0;
+    }
+  }
 }
